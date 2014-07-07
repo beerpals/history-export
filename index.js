fd3e77cffc29ce4bfa07fe0fc31509cd1b3b1bc6
Jan Antala <jan@antala.sk>
2014-07-07 22:35:57 +0200

    feat: chceck if the repo exists

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
 var git = require('./lib/git');
 
-var repo = argv['_'][0] || '';
+repo = fs.existsSync(argv['_'][0]) ? argv['_'][0] : '';
 
 git.getCommits(repo, function(err, commits) {
   if (err) return done('Failed to read git log.\n'+err);
