0bc810af0e20cc1a071dc7721f64276b3a0c44d7
Jan Antala <jan@antala.sk>
2014-07-07 22:42:18 +0200

    feat: get error message

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var git = require('./lib/git');
 repo = fs.existsSync(argv['_'][0]) ? argv['_'][0] : '';
 
 git.getCommits(repo, function(err, commits) {
-  if (err) return done('Failed to read git log.\n'+err);
+  if (err) return new Error('Failed to read git log.\n'+err);
   console.log(commits);
 });
