3a41fdcf802e21251d45af314576952de458fb31
Jan Antala <jan@antala.sk>
2014-07-07 22:49:54 +0200

    feat: sort commits by date

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var git = require('./lib/git');
 
 repo = fs.existsSync(argv['_'][0]) ? argv['_'][0] : '';
 
+var compareByDate = function(a, b) {
+  if (new Date(a.date) < new Date(b.date))
+     return 1;
+  if (new Date(a.date) > new Date(b.date))
+    return -1;
+  return 0;
+};
+
 git.getCommits(repo, function(err, commits) {
   if (err) return new Error('Failed to read git log.\n'+err);
-  console.log(commits);
+  console.log(commits.sort(compareByDate));
 });
